c0afcbcdf7b2ab1a7ae1c60646dbeab42be1025a
Mohamed Akram Zaytar <mohamed.akram.zaytar@ibm.com>
2022-12-30 15:59:28 +0100

    Added requirements to the package.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.md') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [ ]
+requirements = [ 'notebook' ]
 
 test_requirements = [ ]
 
